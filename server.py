1b007f73a38f68880875a0bb6a8deb57c791a44c
timurcumnapiyon <112657086+timurcumnapiyon@users.noreply.github.com>
2024-04-01 19:43:46 +0300

    6 задание
    Модифицируйте код сервера таким образом, чтобы он автоматически изменял номер порта, если он уже занят. Сервер должен выводить в консоль номер порта, который он слушает.

--- server.py
+++ server.py
@@ -1,20 +1,28 @@
 import socket
 
-sock = socket.socket()
-sock.bind(('', 9090))
+def find_free_port(start_port):
+    port = start_port
+    while True:
+        try:
+            sock = socket.socket()
+            sock.bind(('', port))
+            print(f"Server is listening on port {port}")
+            return sock, port
+        except OSError:
+            print(f"Port {port} is already in use, trying the next one...")
+            port += 1
+
+sock, port = find_free_port(9090)
 sock.listen(0)
 conn, addr = sock.accept()
-print(addr)
-
-msg = ''
+print("Client accepted")
+print("Client address:", addr[0])
+print("Client port:", addr[1])
 
 while True:
-	data = conn.recv(1024)
-	if not data:
-		break
-	msg += data.decode()
-	conn.send(data)
-
-print(msg)
+    data = conn.recv(1024)
+    if not data:
+        break
+    conn.send(data.upper())
 
 conn.close()
