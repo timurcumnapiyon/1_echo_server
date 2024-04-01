9110b05a6ea3b60bac1209e5d9a436057eb8c6a4
timurcumnapiyon <112657086+timurcumnapiyon@users.noreply.github.com>
2024-04-01 19:49:19 +0300

    Update client.py

--- client.py
+++ client.py
@@ -4,16 +4,6 @@ host = input("Enter server hostname or IP address: ")
 port = int(input("Enter server port: "))
 
 sock = socket.socket()
-sock.setblocking(1)
-sock.connect(('localhost', 9090))
-
-while True:
-    msg = input("Your string (type 'exit' to quit): ")
-    sock.send(msg.encode())
-    if msg.strip() == 'exit':
-        break
-    data = sock.recv(1024)
-    print(data.decode())
 sock.connect((host, port))
 
 msg = input("Enter your message: ")
